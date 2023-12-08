bafaf26f0f9ef63b67290fbf9611b7460c6c09e6
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2023-12-08 15:48:05 +0200

    added Loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,12 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { Section } from './App.styled';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts } from 'redux/operations';
-import { selectError, selectIsLoading } from 'redux/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -22,9 +19,7 @@ export const App = () => {
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
-      <ContactList>
-        {isLoading && !error && <b>Request in progress...</b>}
-      </ContactList>
+      <ContactList />
     </Section>
   );
 };
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,25 +1,36 @@
+import { Loader } from 'components/Loader';
 import { List, Item, Button } from './ContactList.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from 'redux/operations';
-import { selectVisibleContacts } from 'redux/selectors';
+import {
+  selectError,
+  selectIsLoading,
+  selectVisibleContacts,
+} from 'redux/selectors';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   const contactsByFilter = useSelector(selectVisibleContacts);
 
   const handleDelete = id => dispatch(deleteContact(id));
 
   return (
-    <List>
-      {contactsByFilter.map(({ id, name, phone }) => (
-        <Item key={id}>
-          <p>
-            {name}: {phone}
-          </p>
-          <Button onClick={() => handleDelete(id)}>Delete</Button>
-        </Item>
-      ))}
-    </List>
+    <>
+      {error && <b>Something went wrong... Try reloading the page </b>}
+      {isLoading && !error && <Loader />}
+      <List>
+        {contactsByFilter.map(({ id, name, phone }) => (
+          <Item key={id}>
+            <p>
+              {name}: {phone}
+            </p>
+            <Button onClick={() => handleDelete(id)}>Delete</Button>
+          </Item>
+        ))}
+      </List>
+    </>
   );
 };
